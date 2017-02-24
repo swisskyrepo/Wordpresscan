03c25043b13788eaf63698516278e0316892dca8
Swissky <maxropelewski@gmail.com>
2017-02-24 18:44:35 +0100

    List WP vulnerabilities from JSON files

--- engine/scan.py
+++ engine/scan.py
@@ -13,10 +13,13 @@ class Scan_Engine:
 
 	def __init__(self, wordpress):
 		self.fingerprint_wp_version(wordpress)
+		self.list_wp_vulnerabilities(wordpress, "plugins")
+		self.list_wp_vulnerabilities(wordpress, "themes")
+		self.list_wp_vulnerabilities(wordpress, "wordpresses")
 
 	"""
 	name        : fingerprint_wp_version(wordpress)
-	description : 
+	description : compare hashes of unique files in order to detect the version
 	"""
 	def fingerprint_wp_version(self, wordpress):
 		tree = etree.parse("database/wp_versions.xml")
@@ -24,11 +27,9 @@ class Scan_Engine:
 
 		# Iterating through 'src' file
 		for i in range(len(root)):
-			
-			# !!!! check for '$' $wp-content$ , $wp-plugin$
 
 			# Download file
-			ddl_url  = wordpress.url + root[i].get('src')
+			ddl_url  = (wordpress.url + root[i].get('src') ).replace('$','')
 			ddl_name = "/tmp/" + (root[i].get('src').replace('/','-'))
 			download_file( ddl_url , ddl_name , True ) 
 
@@ -48,3 +49,34 @@ class Scan_Engine:
 						print warning("Advanced fingerprinting detected wp version : %s" % wordpress.version)
 						return
 					
+
+	"""
+	name        : list_wp_vulnerabilities(self, wordpress, file)
+	description : display info about a vulnerability
+	"""
+	def list_wp_vulnerabilities(self, wordpress, file):
+		# Load json file
+		with open('database/'+file+'.json') as data_file:
+			data = json.load(data_file)
+
+		# Extract plugin's infos
+		for key in data.keys():
+
+			# Skip the useless plugins
+			if data[key]["vulnerabilities"] == []:
+				continue
+
+			# Display vulnerabilities
+			print warning("%s: %s" % (file.capitalize(), key) )
+			for vuln in data[key]["vulnerabilities"]:
+
+				print notice("\t%s : %s - ID:%s" % (vuln['vuln_type'], vuln['title'] , vuln['id']) )
+				print info("\tFixed in %s"% vuln['fixed_in'])
+
+				# Display references
+				if 'url' in vuln['references']:
+					print info("\tReferences:")
+					for ref in vuln['references']['url']:
+						print "\t\t -",ref
+
+				print ""
